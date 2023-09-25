2b3015a0f613612c3ed6a68da57ddd12c4753f33
Jessica Twitmyer <jatwitmyer@gmail.com>
2023-09-25 10:57:41 -0400

    connect components to one another

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Visuals from "./Visuals";
+import SoundLibrary from "./SoundLibrary"
+import SavedBeats from "./SavedBeats"
 
 function Display() {
-    <></>
+    <div>
+        <Visuals />
+        <SoundLibrary />
+        <SavedBeats />
+    </div>
 }
 
 export default Display
