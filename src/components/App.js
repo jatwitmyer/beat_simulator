ddc69e50cebe97a4cb3d6c99ee58e872b0288152
keyla <keyyleiva@gmail.com>
2023-09-25 14:19:04 -0400

    add basic css

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Display from "./Display";
 function App() {
   return (
     <div className="App">
+      <br></br>
       <NavBar />
       <Display />
     </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,16 @@ import React from "react";
 
 function NavBar() {
   return(
-    <></>
+    <>
+      <img src="https://i.ibb.co/2K2MfqY/header.png" alt="header" border="0"></img>
+      <br></br>
+      <ul>
+        <li><a class="active" href="#home">Home</a></li>
+        <li><a href="#news">Saved Beats</a></li>
+        <li><a href="#contact">Sound Library</a></li>
+        <li><a href="#about">Mix</a></li>
+      </ul>
+    </>
   )
 }
 
