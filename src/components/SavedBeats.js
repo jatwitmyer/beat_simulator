7f0d4aab6a0493baab538dcc2efdfba37818b92e
Jessica Twitmyer <jatwitmyer@gmail.com>
2023-09-25 15:48:02 -0400

    update filters to reflect database keys

--- src/components/SavedBeats.js
+++ src/components/SavedBeats.js
@@ -4,14 +4,32 @@ import BeatButton from "./BeatButton";
 function SavedBeats( { sounds } ) {
   // console.log(sounds)
 
-  const soundElements = sounds.map(sound => {
+  const edmSounds = sounds.filter(sound => sound.genre === "EDM")
+  const wubstepSounds = sounds.filter(sound => sound.genre === "Wubstep")
+  const trapSounds = sounds.filter(sound => sound.genre === "Trap")
+
+  console.log(edmSounds)
+  console.log(wubstepSounds)
+  console.log(trapSounds)
+
+  const edmElements = edmSounds.map(sound => {
+    return(<BeatButton key={sound.id} sound={sound}/>)
+  })
+  const dubElements = wubstepSounds.map(sound => {
+    return(<BeatButton key={sound.id} sound={sound}/>)
+  })
+  const trapElements = trapSounds.map(sound => {
     return(<BeatButton key={sound.id} sound={sound}/>)
   })
 
   return(
     <div>
-      <h3>All Sounds:</h3>
-      {soundElements}
+      <h3>EDM Sounds:</h3>
+      {edmElements}
+      <h3>Dub Sounds:</h3>
+      {dubElements}
+      <h3>Trap Sounds:</h3>
+      {trapElements}
     </div>
   )
 }
