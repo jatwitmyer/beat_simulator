0373c60627ea22be6da7e003a19b3e7abf5274b5
Jessica Twitmyer <jatwitmyer@gmail.com>
2023-09-28 15:36:30 -0400

    attempt to create a reset database button. not yet functioning

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,8 @@ import TrapPads from "./TrapPads";
 import Tracks from "./Tracks";
 import ResetButton from "./ResetButton";
 import Welcome from "./Welcome";
-import { Switch, Route } from 'react-router-dom'
+import ResetDatabase from "./ResetDatabase";
+import { Switch, Route } from 'react-router-dom';
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
   const [globalMute, setGlobalMute] = useState(false)
   const [beatButtonWasClicked, setBeatButtonWasClicked] = useState(0)
   const [meeples, setMeeples] = useState("")
+  const [resetDatabaseButtonWasClicked, setResetDatabaseButtonWasClicked] = useState(0)
 
   const audioSources = playingSongs.map(beat => {
     return (globalMute ? '' : <Tracks key={beat.id} src={beat.ref} />)
@@ -31,22 +33,43 @@ function App() {
     fetch("http://localhost:8003/sounds")
       .then(r => r.json())
       .then(sounds => setSounds(sounds))
-  }, [beatButtonWasClicked])
+  }, [beatButtonWasClicked, resetDatabaseButtonWasClicked])
 
-  useEffect(()=>{
+  useEffect(() => {
     setMeeples(playingSongs.length > 0 ? playingSongs[0].genre : "still")
   }, [playingSongs])
 
+  function resetDatabase() {
+    console.log("Reset button clicked")
+    // const resetSounds = sounds.map(sound => {
+    sounds.forEach(sound => {
+      fetch(`http://localhost:8003/sounds/${sound.id}`, {
+        method: "PATCH",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          "genre": sound.genre,
+          "type": sound.type,
+          "name": sound.name,
+          "ref": sound.ref,
+          "isMuted": true
+        })
+      })
+        .then(r => r.json())
+        .then(data => console.log(data))
+    })
+    setResetDatabaseButtonWasClicked(resetDatabaseButtonWasClicked + 1)
+  }
+
   return (
     <div className="App">
-      <NavBar muteSwitch={muteSwitch}/>
+      <NavBar muteSwitch={muteSwitch} />
       <Switch>
         <Route exact path="/">
           <Welcome />
         </Route>
         <Route exact path="/mixer">
           <div className="mixer">
-            <Visuals meeples={meeples}/>
+            <Visuals meeples={meeples} />
             <div className="padscontainer">
               <SavedBeats
                 sounds={sounds}
@@ -61,10 +84,16 @@ function App() {
             </div>
             {audioSources}
           </div>
+          <div className="reset-database">
+            <ResetDatabase
+              resetDatabase={resetDatabase}
+              setResetDatabaseButtonWasClicked={setResetDatabaseButtonWasClicked}
+            />
+          </div>
         </Route>
         <Route exact path="/edm">
           <div className="mixer">
-            <Visuals meeples={meeples}/>
+            <Visuals meeples={meeples} />
             <div className="padscontainer">
               <EdmPads
                 sounds={sounds}
@@ -77,10 +106,16 @@ function App() {
               {audioSources}
             </div>
           </div>
+          <div className="reset-database">
+            <ResetDatabase
+              resetDatabase={resetDatabase}
+              setResetDatabaseButtonWasClicked={setResetDatabaseButtonWasClicked}
+            />
+          </div>
         </Route>
-        <Route exact path="/wubstep">
+        <Route exact path="/trap">
           <div className="mixer">
-            <Visuals meeples={meeples}/>
+            <Visuals meeples={meeples} />
             <div className="padscontainer">
               <TrapPads
                 sounds={sounds}
@@ -93,10 +128,16 @@ function App() {
               {audioSources}
             </div>
           </div>
+          <div className="reset-database">
+            <ResetDatabase
+              resetDatabase={resetDatabase}
+              setResetDatabaseButtonWasClicked={setResetDatabaseButtonWasClicked}
+            />
+          </div>
         </Route>
-        <Route exact path="/trap">
+        <Route exact path="/wubstep">
           <div className="mixer">
-            <Visuals meeples={meeples}/>
+            <Visuals meeples={meeples} />
             <div className="padscontainer">
               <WubPads
                 sounds={sounds}
@@ -108,6 +149,12 @@ function App() {
               <ResetButton muteSwitch={muteSwitch} />
               {audioSources}
             </div>
+          </div>
+          <div className="reset-database">
+            <ResetDatabase
+              resetDatabase={resetDatabase}
+              setResetDatabaseButtonWasClicked={setResetDatabaseButtonWasClicked}
+            />
           </div>
         </Route>
       </Switch>
---
+++ src/components/ResetDatabase.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ResetDatabase({resetDatabase}) {
+  return (
+    <button onClick={resetDatabase}>Reset Database</button>
+  )
+}
+
+export default ResetDatabase
